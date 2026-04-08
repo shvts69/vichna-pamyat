946c7a3982e4eb3a07bf19182baf6dc9ecb52d44
Ross Shvts <rossshvts@MacBookPro.lan>
2026-04-07 21:50:31 -0400

    Оновлено домен на vichna-pamyat.com.ua

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://vichna-pamyat.vercel.app/sitemap.xml",
+    sitemap: "https://vichna-pamyat.com.ua/sitemap.xml",
   };
 }
